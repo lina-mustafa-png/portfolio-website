533d576a5e283dcc1caff629d553f445cb0becf7
Lina Mustafa <lina9.mustafa90@gmail.com>
2026-08-31 15:54:10 -0400

    style changes

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
+const projects = document.getElementById("projectsbtn");
 
-
-btn1.addEventListener("click", function () {
-    window.location.href = "python.html";
-});
-
-btn2.addEventListener("click", function () {
-    window.location.href = "html.html";
-});
-
-btn3.addEventListener("click", function () {
-    window.location.href = "java.html";
-});
-
-btn4.addEventListener("click", function () {
-    window.location.href = "c.html";
+projects.addEventListener("click", function () {
+    window.location.href = "projects.html";
 });
