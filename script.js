28da63fe3a64148b973f7221f985271a6d359489
Lina Mustafa <lina9.mustafa90@gmail.com>
2026-09-02 22:39:26 -0400

    new pages

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const about = document.getElementById("aboutbtn");
 const projects = document.getElementById("projectsbtn");
+const skills = document.getElementById("skillsbtn");
+const contact = document.getElementById("contactbtn");
+
+projects.addEventListener("click", function () {
+    window.location.href = "about.html";
+});
 
 projects.addEventListener("click", function () {
     window.location.href = "projects.html";
+});
+
+projects.addEventListener("click", function () {
+    window.location.href = "skills.html";
+});
+
+projects.addEventListener("click", function () {
+    window.location.href = "contact.html";
 });
